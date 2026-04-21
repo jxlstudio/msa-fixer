44c7a16d42e0edc37c8684396310aa997350e73d
JXL Studio <jeremy@jxl.io>
2026-04-21 15:11:03 -0400

    updated toast style

--- app/app.vue
+++ app/app.vue
@@ -262,9 +262,12 @@
 
     <div
       v-if="copyMessage"
-      class="fixed bottom-4 right-4 bg-black px-4 py-2 text-xl text-white shadow"
+      class="fixed top-4 right-4 z-9999"
     >
-      {{ copyMessage }}
+      <CyberFrame
+      variant="fuchsia">
+        {{ copyMessage }}
+      </CyberFrame>
     </div>
   </div>
 </template>
